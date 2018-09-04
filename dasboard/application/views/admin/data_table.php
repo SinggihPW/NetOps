c041c47eeee8504cd2d88eec0389103fed8b81fe
SinggihPW <singgihprayogo24@gmail.com>
2018-09-04 13:35:46 +0700

    chart
    penambahan controller chart

--- dasboard/application/views/admin/data_table.php
+++ dasboard/application/views/admin/data_table.php
@@ -3,12 +3,12 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Simple Tables
-        <small>preview of simple tables</small>
+        Dashboard
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Tables</a></li>
+        </section>
+        <!--<li><a href="#">Tables</a></li>
         <li class="active">Simple</li>
       </ol>
     </section>
